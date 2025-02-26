75cfb34711828fcb74bdefc5ea27cce97aba4cdf
Noora Lankinen <147394889+nooralankinen@users.noreply.github.com>
2025-02-26 08:49:24 +0200

    Update tapahtumat.js

---
+++ tapahtumat.js
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+
+<html lang="fi">
+
+<head>
+    <title>JSON Example</title> 
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="tyyli.css">
+    <ul>
+        <li><a href="kurssi.html">Home</a></li>
+        <li><a href="toteutus.html">Toteutus</a></li>
+        <li><a href="saa.html">Sää</a></li>
+        <li><a href="tapahtumat.html">Tapahtumat</a></li>
+    </ul>
+</head>
+
+<body>
+    <h1>Tapahtumat Tampereella</h1>
+
+    <div id="vastaus"> </div>     
+
+    <script src="tapahtumat.js"></script>
+
+    
+    
+</body>
+</html>
