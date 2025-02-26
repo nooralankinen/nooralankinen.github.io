bab938ec4672c02eebb64635f525d89771669daa
Noora Lankinen <147394889+nooralankinen@users.noreply.github.com>
2025-02-26 09:00:02 +0200

    Update tapahtumat.js

--- tapahtumat.js
+++ tapahtumat.js
@@ -1,28 +1,34 @@
-<!DOCTYPE html>
+// Funktio
 
-<html lang="fi">
-
-<head>
-    <title>JSON Example</title> 
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="tyyli.css">
-    <ul>
-        <li><a href="kurssi.html">Home</a></li>
-        <li><a href="toteutus.html">Toteutus</a></li>
-        <li><a href="saa.html">Sää</a></li>
-        <li><a href="tapahtumat.html">Tapahtumat</a></li>
-    </ul>
-</head>
-
-<body>
-    <h1>Tapahtumat Tampereella</h1>
-
-    <div id="vastaus"> </div>     
-
-    <script src="tapahtumat.js"></script>
-
+    function tapahtumat(data) {
+        var teksti = "";
+        teksti = "<h1>Tampereella tapahtuu</h1>";
+        for (var i = 0; i < data.length; i++) {
+        teksti = teksti + "<h3>" + data[i].name + "</h3>";
+        teksti = teksti + "<p>" + data[i].description + "</p>";
+        teksti = teksti + `<p><a href="${data[i].url}">${data[i].url}</a></p>`;
+        }
     
+    document.getElementById("d44").innerHTML = teksti;
     
-</body>
-</html>
+    }
+// määritetään sivu, josta tapahtumat haetaan
+
+    fetch('https://api.visittampere.com/api/v1/visittampere/event/published/all/?format=json&lang=fi')
+
+// Muunnetaan vastaus JSON muotoon
+
+    .then(function (response) { return response.json();})
+
+// Käsitellään muunnettu (eli JSON muotoinen) vastaus
+
+    .then(function (data) {
+        tapahtumat(data);
+
+    })
+
+// Jos tuli jokin virhe
+
+    .catch(function (error) { document.getElementById("vastaus").innerHTML = "<p>Tietoa ei pystytä hakemaan </p>"+ error;
+
+    })
