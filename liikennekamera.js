6bb439fea89b5696d1f69555f7771c0d0d5516df
Noora Lankinen <147394889+nooralankinen@users.noreply.github.com>
2025-02-26 11:57:48 +0200

    Update liikennekamera.js

--- liikennekamera.js
+++ liikennekamera.js
@@ -1,5 +1,5 @@
 /*API*/
-fetch(`https://cors-anywhere.herokuapp.com/https://tie.digitraffic.fi/api/weathercam/v1/stations/C04507/data`)
+fetch(`https://tie.digitraffic.fi/api/weathercam/v1/stations/C04507/data`)
 
 /*Muunnetaan vastaus JSON muotoon*/
 .then(function(response) {
