619970fea8c159341a66310eb2818e82b548f2e8
Noora Lankinen <147394889+nooralankinen@users.noreply.github.com>
2025-02-26 11:49:51 +0200

    Update liikennekamera.js

--- liikennekamera.js
+++ liikennekamera.js
@@ -1,33 +1,26 @@
-
-
 /*API*/
-    fetch(`"https://cors-anywhere.herokuapp.com/https://tie.digitraffic.fi/api/weathercam/v1/stations/C04507/data"`)
+fetch("https://cors-anywhere.herokuapp.com/https://tie.digitraffic.fi/api/weathercam/v1/stations/C04507/data")
 
 /*Muunnetaan vastaus JSON muotoon*/
-
-    .then(function (response) { return response.json();})
+.then(function(response) {
+    return response.json();
+})
 
 /* Käsitellään muunnettu (eli JSON muotoinen) vastaus */
+.then(function(data) {
+    kuvat(data);
+})
 
-    .then(function (data) {
-        kuvat(data);
+/* Jos tuli jokin virhe */
+.catch(function(error) { 
+    document.getElementById("kamerat").innerHTML = "<p>Tietoa ei pystytä hakemaan </p>";
+});
 
-    })
-
-/* Jos tuli jokin virhe*/
-
-    .catch(function (error) { 
-        document.getElementById("kamerat").innerHTML = "<p>Tietoa ei pystytä hakemaan </p>";
-    })
-
-/*Näytetään yhden kameran kuva*/
-
+/*Näytetään yhden kameran kuva */
 function kuvat(data) {
     document.getElementById("kuvat");
 
-
-/*Käydään läpi kaikki kuvat for-loopilla*/
-
+    /* Käydään läpi kaikki kuvat for-loopilla */
     for (var i = 0; i < data.presets.length; i++) {
         const camera = data.presets[i];
         const kuvaElementti = document.createElement("div");
@@ -35,13 +28,8 @@ function kuvat(data) {
             <h3>Kamera ID: ${camera.id}</h3>
             <p>Aika: ${camera.measuredTime}</p>
             <img src="https://tie.digitraffic.fi/api/weathercam/v1/stations/${data.id}/data/${camera.id}/image" alt="Liikennekamera ${camera.id}" width="400"><hr>`;
-    
-/*Kootaan kuvat elementtiin*/
 
-document.getElementById("kuvat").appendChild(kuvaElementti);
+        /* Kootaan kuvat elementtiin */
+        document.getElementById("kuvat").appendChild(kuvaElementti);
     }
 }
-    
-
-
-
